13c41efff6724c3cb150bf9bd86ab3a321ad3b58
Anushka Thakkar <59010980+AnushkaThakkar@users.noreply.github.com>
2020-10-04 00:43:10 +0530

    Updated

--- Sorting Algorithm/Merge_Sort_Algorithm/Merge_Sort_input.py
+++ Sorting Algorithm/Merge_Sort_Algorithm/Merge_Sort_input.py
@@ -1,15 +1,15 @@
 # Author: AnushkaThakkar
 
-# This program receives the list as an input from User and then sort it using merge sort algorithm.
+# This program receives an list of numbers as an input from User and then sort it using merge sort algorithm.
 
-def merge_sort(alist, start, end):       # <--- merge_sort will sort the list from indexes start to end – 1 inclusive
+def merge_sort(alist, start, end):       # <--- merge_sort will sort the list of numbers from indexes start to end – 1 inclusive
     if end - start > 1:                  # <--- If end – start is not greater than 1, then return
         mid = (start + end)//2           # <--- Otherwise, set mid equal to the floor of (start + end)/2
-        merge_sort(alist, start, mid)    # <--- Call merge_sort with the same list and with start = start and end = mid as arguments
-        merge_sort(alist, mid, end)      # <--- Call merge_sort with the same list and with start = mid and end = end as arguments
+        merge_sort(alist, start, mid)    # <--- Call merge_sort with the same list of numbers and with start = start and end = mid as arguments
+        merge_sort(alist, mid, end)      # <--- Call merge_sort with the same list of numbers and with start = mid and end = end as arguments
         merge_list(alist, start, mid, end)   
  
-def merge_list(alist, start, mid, end):    # <--- merges to create a new sorted list from indexes start to end – 1.
+def merge_list(alist, start, mid, end):    # <--- merges to create a new sorted list of numbers from indexes start to end – 1.
     left = alist[start:mid]
     right = alist[mid:end]
     k = start
@@ -35,8 +35,8 @@ def merge_list(alist, start, mid, end):    # <--- merges to create a new sorted 
             k = k + 1
  
  
-alist = input('Enter the list of numbers: ').split()   # <--- Receiving the list from user
+alist = input('Enter the list of numbers: ').split()   # <--- Receiving the list of numbers from user
 alist = [int(x) for x in alist]
 merge_sort(alist, 0, len(alist))                       # <--- Calling merge_sort function
 print('Sorted list: ', end='')            
-print(alist)                                           # <--- Printing sorted list
+print(alist)                                           # <--- Printing sorted list of numbers
