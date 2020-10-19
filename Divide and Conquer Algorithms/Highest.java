d4ef3360358c6f95728740b4b9cac14a37c998e0
zflash123 <z4ed.thalib123@gmail.com>
2020-10-19 08:34:03 +0700

    add Highest.java

---
+++ Divide and Conquer Algorithms/Highest.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Highest{
+	public static void main(String[] args){
+		Scanner inputZaed = new Scanner(System.in);
+		int num1, num2;
+		System.out.println("Input first number : ");
+		num1 = inputZaed.nextInt();
+		System.out.println("Input second number : ");
+		num2 = inputZaed.nextInt();
+		if (angka1>=angka2){
+			System.out.println("The biggest number is "+num1);
+		}
+		else{
+			System.out.println("The biggest number is "+num2);
+		}
+	}ng
+}
