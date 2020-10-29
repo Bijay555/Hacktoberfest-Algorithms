1ab564f2252dfe8be926fcfc5a84ded46aa44434
Georgiy <dev.georgiy@gmail.com>
2020-10-29 23:56:05 +0300

    base struct of src file binary_search.cpp

---
+++ Divide and Conquer Algorithms/binary_search.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cassert>
+
+/**
+ * Simple non-recursive implementation of binary search (search in a sorted array).
+ * Log(n) complexity :)
+ * @param arr - array to search for
+ * @param arrSize - array size
+ * @param el - element to search for
+ * @return the index of the element or -1 in case of failure
+ */
+int binarySearch(int *arr, size_t arrSize, int el) {
+    assert(arr);
+    for (int i = 0; i < ; ++i) {
+        
+    }
+    
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
